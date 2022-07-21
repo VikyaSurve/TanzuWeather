909a261034d04c43a49f61e29951d0a8cf4438a7
Vikas Surve <VikyaSurve@users.noreply.github.com>
2022-07-21 14:18:49 +0530

    Build Excuted successfully

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Weather.Models;
 
 namespace Weather.Controllers
 {
@@ -25,6 +28,47 @@ namespace Weather.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public String WeatherDetail(string City)
+        {
+
+            //Assign API KEY which received from OPENWEATHERMAP.ORG  
+            string appId = "8113fcc5a7494b0518bd91ef3acc074f";
+
+            //API path with CITY parameter and other parameters.  
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+ 
+                //Converting to OBJECT from JSON string.  
+                RootObject weatherInfo = JsonConvert.DeserializeObject<RootObject>(json);
+
+                //Special VIEWMODEL design to send only required fields not all fields which received from   
+                //www.openweathermap.org api  
+                ResultViewModel rslt = new ResultViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                //Converting OBJECT to JSON String   
+                var jsonstring = JsonConvert.SerializeObject(rslt);
+
+                //Return JSON string.  
+                return jsonstring;
+            }
         }
     }
 }
